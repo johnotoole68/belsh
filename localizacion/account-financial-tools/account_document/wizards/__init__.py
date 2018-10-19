e051e4deacba515e538c459b3a2908a3b58bb1c4
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-10-19 11:55:30 -0300

    Cleaning

--- localizacion/account-financial-tools/account_document/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_invoice_refund
-from . import account_journal_merge_wizard
--- localizacion/account-payment/account_withholding/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-from . import account_tax
-from . import account_payment
-from . import account_chart_template
