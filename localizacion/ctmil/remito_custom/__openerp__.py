747cc460d08ec9001eca35e5531aaf9d09ff80a5
Ignacio Buioli <ibuioli@gmail.com>
2019-04-09 15:52:52 -0300

    feat: new module - remito_custom

---
+++ localizacion/ctmil/remito_custom/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#       Pere Ramon Erro Mas <pereerro@tecnoba.com> All Rights Reserved.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Report Custom',
+    'version': '1.0',
+    'author': 'Moldeo Interactive Coop. Ltda.',
+    'website': 'https://www.moldeointeractive.com.ar',
+    "category": "Sales",
+    "depends": ['base', 'product','sale','website_sale','stock'],
+    'data': [
+        'views/report.xml',
+    ],
+    'demo_xml': [],
+    'active': False
+}
---
+++ localizacion/ctmil/remito_custom/controllers/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import base64
+from odoo import http, api
+from odoo import fields, osv
+from odoo.http import request
+
+#class ReportCustom(http.Controller):
