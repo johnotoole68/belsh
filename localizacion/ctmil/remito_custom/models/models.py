9f5e9b355a4f64eeec31ec29f5ba93509f9f0ac4
Ignacio Buioli <ibuioli@gmail.com>
2019-04-10 15:42:51 -0300

    test: quant_ids

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -7,10 +7,16 @@ class BelshStockPicking(models.Model):
 	@api.multi
 	def _barrels_stock(self):
 		barrels_ids = self.env['stock.production.lot'].search([('product_id', '=', 159)])
+		quant_ids = []
 		if barrels_ids:
-			raise ValidationError(barrels_ids)
+			for rec in self:
+				for barrel in barrels_ids:
+					if barrel.quant_ids:
+						quant_ids.append(barrel.quant_ids)
+				raise ValidationError(quant_ids)
 		else:
-			raise ValidationError('Testing Compute')
+			for rec in self:
+				rec.barril = ''
 
 	barril = fields.Char('Barriles', compute=_barrels_stock)
 
