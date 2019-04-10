31413ab50e3861ab475fab6406ec9ac1c934ced3
Ignacio Buioli <ibuioli@gmail.com>
2019-04-10 15:51:12 -0300

    test: location_id

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -8,12 +8,20 @@ class BelshStockPicking(models.Model):
 	def _barrels_stock(self):
 		barrels_ids = self.env['stock.production.lot'].search([('product_id', '=', 159)])
 		quant_ids = []
+		locs = []
+
 		if barrels_ids:
 			for rec in self:
 				for barrel in barrels_ids:
 					if barrel.quant_ids:
 						quant_ids.append(barrel.quant_ids)
-				raise ValidationError(quant_ids)
+
+				if quant_ids:
+					for qids in quant_ids:
+						for qid in qids:
+							if qid.location_id == 9:
+								locs.append(qid.location_id)
+				raise ValidationError(locs)
 		else:
 			for rec in self:
 				rec.barril = ''
