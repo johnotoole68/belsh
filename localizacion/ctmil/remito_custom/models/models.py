0d7ffe916406eef1a55049a25900632ee86382a7
Ignacio Buioli <ibuioli@gmail.com>
2019-04-10 15:30:59 -0300

    feat: testing search on stock.production.lot

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -6,10 +6,11 @@ class BelshStockPicking(models.Model):
 
 	@api.multi
 	def _barrels_stock(self):
-		#barrels_ids = self.env['stock.production.lot'].search(['product_id', '=', 159])
-		raise ValidationError('Probando Computacion')
-		for rec in self:
-			rec.barril = 'Hola Mundo'
+		barrels_ids = self.env['stock.production.lot'].search([('product_id', '=', 159)])
+		if barrels_ids:
+			raise ValidationError(barrels_ids)
+		else:
+			raise ValidationError('Testing Compute')
 
 	barril = fields.Char('Barriles', compute=_barrels_stock)
 
