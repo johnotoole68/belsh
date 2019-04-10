7fc1c739ec17046720c4fcc728aed0e3ef481364
Ignacio Buioli <ibuioli@gmail.com>
2019-04-10 15:17:50 -0300

    fix: view & compute field test

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -5,9 +5,11 @@ class BelshStockPicking(models.Model):
 	_inherit = "stock.picking"
 
 	@api.multi
-	def barrels_stock(self):
-		barrels_ids = self.env['stock.production.lot'].search(['product_id', '=', 159])
-		raise ValidationError(barrels_ids)
+	def _barrels_stock(self):
+		#barrels_ids = self.env['stock.production.lot'].search(['product_id', '=', 159])
+		raise ValidationError('Probando Computacion')
+		for rec in self:
+			rec.barril = 'Hola Mundo'
 
-	barril = fields.Char('Barriles', compute="barrels_stock")
+	barril = fields.Char('Barriles', compute=_barrels_stock)
 
