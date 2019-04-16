0bb491f262050d3746c450af593ccecd3ed5e9e8
Ignacio Buioli <ibuioli@gmail.com>
2019-04-16 18:05:30 -0300

    test: recursitivy #5 rais #1

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -23,12 +23,12 @@ class BelshStockPicking(models.Model):
 
 					if quant_ids:
 						if quant_ids[-1].location_id.id == 9:
-								locs.append(quant_ids[-1])
-
-					raise ValidationError(locs)
+							locs.append(quant_ids[-1])
 
 					if locs:
-						if locs.history_ids[-1].location_dest_id.id == 9:
+						if locs.history_ids:
+							raise ValidationError(locs.history_ids)
+							if locs.history_ids[-1].location_dest_id.id == 9:
 								barrel_data.append(history_ids[-1])
 
 					if barrel_data:
