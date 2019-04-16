e606d99e0eb75d2b98c86f79f8839b3eaf691c28
Ignacio Buioli <ibuioli@gmail.com>
2019-04-16 13:56:09 -0300

    fix: recursivity #1

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -38,12 +38,13 @@ class BelshStockPicking(models.Model):
 									barrel_data.append(h_res[-1])
 
 					if barrel_data:
-						table = '<table><tr><th>Orden de Venta</th><th>Nro Factura</th><th>Fecha Factura</th><th>Nro Remito</th><th>Nro Barril</th><th>Antiguedad</th><th>Retira</th><th>Firma</th></tr>'
-						for b in barrel_data:
-							if b.partner_id.id == self.partner_id.id:
-								table = table + '<tr><td>' + str(b.origin) + '</td><td> </td><td> </td><td>' + str(b.picking_id.name) + '</td><td>' + str(nro_barril) + '</td><td></td><td> </td><td> </td></tr>'
+						if barrel_data.partner_id.id == rec.partner_id.id:
+							table = '<table><tr><th>Orden de Venta</th><th>Nro Factura</th><th>Fecha Factura</th><th>Nro Remito</th><th>Nro Barril</th><th>Antiguedad</th><th>Retira</th><th>Firma</th></tr>'
+							table = table + '<tr><td>' + str(b.origin) + '</td><td> </td><td> </td><td>' + str(b.picking_id.name) + '</td><td>' + str(nro_barril) + '</td><td></td><td> </td><td> </td></tr>'
 
-						rec.barril = table + '</table>' 
+							rec.barril = table + '</table>'
+					else:
+						rec.barril = '' 
 
 		else:
 			for rec in self:
