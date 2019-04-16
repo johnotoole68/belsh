8128ccf22f28db27b74799bed86f8adc8fb41368
Ignacio Buioli <ibuioli@gmail.com>
2019-04-16 17:38:54 -0300

    test: recursivity #2

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -37,14 +37,14 @@ class BelshStockPicking(models.Model):
 								if h_res[-1].location_dest_id.id == 9:
 									barrel_data.append(h_res[-1])
 
-					if barrel_data:
+					"""if barrel_data:
 						if barrel_data.partner_id == rec.partner_id:
 							table = '<table><tr><th>Orden de Venta</th><th>Nro Factura</th><th>Fecha Factura</th><th>Nro Remito</th><th>Nro Barril</th><th>Antiguedad</th><th>Retira</th><th>Firma</th></tr>'
 							table = table + '<tr><td>' + str(b.origin) + '</td><td> </td><td> </td><td>' + str(b.picking_id.name) + '</td><td>' + str(nro_barril) + '</td><td></td><td> </td><td> </td></tr>'
 
 							rec.barril = table + '</table>'
 					else:
-						rec.barril = ''
+						rec.barril = ''"""
 
 		else:
 			for rec in self:
