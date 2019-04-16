33cac1c382f01840841c97f7addf6d92a951278e
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-04-16 17:10:32 -0300

    From scratch

--- localizacion/account-financial-tools/account_move_helper/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import account_account
-from . import account_move
-from . import res_partner
--- localizacion/ctmil/remito_custom/controllers/main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import base64
-from odoo import http, api
-from odoo import fields, osv
-from odoo.http import request
-
-#class ReportCustom(http.Controller):
