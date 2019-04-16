40c6b5e59fef336088ce18927875bdeb980d23c8
Ignacio Buioli <ibuioli@gmail.com>
2019-04-16 18:40:17 -0300

    test: recursitivy #10.1

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -34,7 +34,7 @@ class BelshStockPicking(models.Model):
 							barrel_data.append(locs[-1])
 
 					if barrel_data:
-						table = table + " " + str(barrel_data.partner_id.id)
+						table = table + " " + str(barrel_data[0].partner_id.id)
 					"""if barrel_data:
 						if barrel_data.partner_id == rec.partner_id:
 							table = '<table><tr><th>Orden de Venta</th><th>Nro Factura</th><th>Fecha Factura</th><th>Nro Remito</th><th>Nro Barril</th><th>Antiguedad</th><th>Retira</th><th>Firma</th></tr>'
