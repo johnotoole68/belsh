99ac52a065fd63ab2b06dc19583ec381401cdc48
Ignacio Buioli <ibuioli@gmail.com>
2019-04-16 19:24:32 -0300

    test: name & nro_barril

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -34,7 +34,7 @@ class BelshStockPicking(models.Model):
 
 					if barrel_data:
 						#if rec.partner_id.id == barrel_data[0].partner_id.id
-						table = table + " " + str(barrel_data[0].origin)
+						table = table + " " + str(barrel_data[0].origin) + ", " + str(nro_barril) + ";"
 					"""if barrel_data:
 						if barrel_data.partner_id == rec.partner_id:
 							table = '<table><tr><th>Orden de Venta</th><th>Nro Factura</th><th>Fecha Factura</th><th>Nro Remito</th><th>Nro Barril</th><th>Antiguedad</th><th>Retira</th><th>Firma</th></tr>'
