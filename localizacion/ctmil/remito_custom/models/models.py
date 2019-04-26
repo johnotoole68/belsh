121d7bbd494c603b14a8bc9c7a72c5b3f3f288e2
Ignacio Buioli <ibuioli@gmail.com>
2019-04-26 13:04:49 -0300

    fix: date to datetime

--- localizacion/ctmil/remito_custom/models/models.py
+++ localizacion/ctmil/remito_custom/models/models.py
@@ -40,7 +40,7 @@ class BelshStockPicking(models.Model):
 							if account:
 								inv_num = account.document_number
 								date_invoice = account.date_invoice
-								delta = date.today() - datetime.strptime(date_invoice, '%Y-%m-%d') 
+								delta = datetime.today() - datetime.strptime(date_invoice, '%Y-%m-%d') 
 								days = str(delta.days)
 
 							table = table + '<tr><td>' + str(barrel_data[0].origin) + '</td><td>' + inv_num + '</td><td>' + str(date_invoice) + '</td><td>' + str(barrel_data[0].picking_id.name) + '</td><td>' + str(nro_barril) + '</td><td>' + days + '</td><td> </td><td> </td></tr>'
